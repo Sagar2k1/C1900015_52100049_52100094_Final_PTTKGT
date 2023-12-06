40009c6d80942672f0b48d4ecfaa28ba8129aef1
Sagar2k1 <84672270+Sagar2k1@users.noreply.github.com>
2023-12-06 23:46:19 +0700

    init project

---
+++ Item.java
@@ -0,0 +1,10 @@
+public class Item {
+    public static int weight;
+    public static int value;
+
+    public Item(int weight, int  height){
+        this.weight = weight;
+        this.value = value;
+    }
+    
+}
